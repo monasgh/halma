db5dda19f9a5f8508f6d4fa33cbc1ae9fb0e3083
mona <sghfim@gmail.com>
2020-12-28 18:09:19 +0330

    change turn

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-//in tabe vorudi ra khande
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+#include<stdio.h>
+//in tabe nobat ra avaz karde va rang har bazikon ra change mikonad
+
+    void next_turn (int &turn){
+	if(turn == 1){
+		turn = 2;
+		system("COLOR 0B");
+        printf("player B");
+
+	}else{
+		turn = 1;
+		system("COLOR 0C");
+        printf("player A");
+
+	}
 }
+
+
+
